71a41765de131941474a5affacc8561eb30d1d5f
ROKN-EMW <seoseo471@gmail.com>
2026-03-28 13:39:53 +0900

    HElp

---
+++ week4/Project3/4-3.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main(void)
+{
+	for (int i = 1; i <= 1000; i++) {
+		cout << setw(a) << i << setw(r) << (i * i) << endl;
+	}
+	return 0;
+}
+
